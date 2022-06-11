6f441c163ab655a2613068732b3d7398a6e7c074
戈晓伟 <gandxiaowei@gmail.com>
2022-06-12 01:41:49 +0800

    :sparkles: add tray

--- src/background.js
+++ src/background.js
@@ -1,18 +1,58 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, BrowserWindow, nativeImage, protocol, Tray } from 'electron'
+import path from 'path'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
+let win, tray
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+  {
+    scheme: 'app',
+    privileges: {
+      secure: true,
+      standard: true
+    }
+  }
 ])
+
+function createTray () {
+  const image = nativeImage.createFromPath(
+    path.join(__dirname, '../src/assets/logo.png')
+  )
+  tray = new Tray(image.resize({
+    width: 16,
+    height: 16
+  }))
+  tray.setToolTip('Nightscount')
+  tray.setTitle('6.4mmol/l')
+  tray.on('right-click', toggleWindow)
+  tray.on('double-click', toggleWindow)
+  tray.on('click', function (event) {
+    toggleWindow()
+
+    // Show devtools when command clicked
+    if (win.isVisible() && process.defaultApp && event.metaKey) {
+      win.openDevTools({ mode: 'detach' })
+    }
+  })
+}
+
+const toggleWindow = () => {
+  if (win.isVisible()) {
+    win.hide()
+  } else {
+    showWindow()
+  }
+}
 
 async function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -31,7 +71,31 @@ async function createWindow () {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    await win.loadURL('app://./index.html')
+  }
+  win.setTitle('Capvision Salary Tool')
+}
+
+const showWindow = () => {
+  const position = getWindowPosition()
+  win.setPosition(position.x, position.y, false)
+  win.show()
+  win.focus()
+}
+
+const getWindowPosition = () => {
+  const windowBounds = win.getBounds()
+  const trayBounds = tray.getBounds()
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+
+  return {
+    x,
+    y
   }
 }
 
@@ -44,10 +108,12 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
+app.on('activate', async () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) {
+    await createWindow()
+  }
 })
 
 // This method will be called when Electron has finished
@@ -62,7 +128,8 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
-  createWindow()
+  createTray()
+  await createWindow()
 })
 
 // Exit cleanly on request from parent process in development mode.
